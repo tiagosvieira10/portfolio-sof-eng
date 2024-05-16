6564f731e8405a7940949fdd7254de29dc2f4e18
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-05-16 12:11:40 -0300

    projects criada e estilizada faltando add projects

---
+++ img/projetosLive/index.tsx
@@ -0,0 +1,18 @@
+export { default as P1 } from './2.svg';
+export { default as P2 } from './1.svg';
+export { default as P3 } from './3.svg';
+export { default as P4 } from './4.svg';
+export { default as P5 } from './5.svg';
+export { default as P6 } from './6.svg';
+export { default as P7 } from './7.svg';
+export { default as P8 } from './8.svg';
+export { default as P9 } from './9.svg';
+export { default as P10 } from './10.svg';
+export { default as P11 } from './11.svg'
+export { default as P12 } from './12.svg'
+export { default as P13 } from './13.svg'
+export { default as P14 } from './14.svg'
+export { default as P15 } from './15.svg'
+
+
+
